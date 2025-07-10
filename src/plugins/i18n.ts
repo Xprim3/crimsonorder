b20a72e509f0942ea1ec1c1f17fa0ae98e8e8e3e
Denis Januzi <denisjanuzi@gmail.com>
2025-07-10 10:04:28 +0200

    update

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,11 +2,11 @@ import { createI18n } from 'vue-i18n'
 
 function loadLocaleMessages() {
   const locales = import.meta.glob('../locales/*.json', { eager: true })
-  const messages: Record<string, object> = {}
+  const messages: { [key: string]: any } = {}
   for (const path in locales) {
     const matched = path.match(/([A-Za-z0-9-_]+)\.json$/i)
     if (matched) {
-      messages[matched[1]] = (locales[path] as { default: object }).default
+      messages[matched[1]] = (locales[path] as any).default
     }
   }
   return messages
