ed44a72803e8fb1938d403c45120d56eb7535de7
Denis Januzi <denisjanuzi@gmail.com>
2025-07-10 21:29:19 +0200

    update

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -81,22 +81,6 @@
               />
             </svg>
           </a>
-          <!-- YouTube -->
-          <a
-            href="#"
-            aria-label="Visit our YouTube channel"
-            class="w-8 h-8 bg-red-800/50 rounded-full flex items-center justify-center hover:bg-red-700/70 transition-all duration-300 border border-red-700/50 hover:border-yellow-500/50 group"
-          >
-            <svg
-              class="w-4 h-4 text-red-400 group-hover:text-yellow-400 transition-colors duration-300"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"
-              />
-            </svg>
-          </a>
         </div>
       </div>
 
---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Layout from '../layout/Layout.vue'
+import HeroSection from '../sections/HeroSection.vue'
+import AboutSection from '../sections/AboutSection.vue'
+import JoinSection from '../sections/JoinSection.vue'
+import JoinFamilySection from '../sections/JoinFamilySection.vue'
+// import KingdomSection from '../sections/KingdomSection.vue'
+import FAQSection from '../sections/FAQSection.vue'
+import DevTeamSection from '../sections/DevTeamSection.vue'
+</script>
+
+<template>
+  <Layout>
+    <HeroSection />
+    <AboutSection />
+    <JoinSection />
+    <JoinFamilySection />
+    <DevTeamSection />
+    <FAQSection />
+    <!-- <KingdomSection v-if="false" /> -->
+  </Layout>
+</template> 
